233833ad5f0eddec43a287c4f659cb543a38dffe
juliossena <juliosouzasena@gmail.com>
2021-12-17 18:46:38 -0300

    feat: insert functions in service desafios

--- src/desafios/desafios.controller.ts
+++ src/desafios/desafios.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { DesafiosService } from './desafios.service';
+import { CriarDesafioDto } from './dtos/criar-desafio.dto';
+import { Desafio } from './interfaces/desafio.interface';
 
 @Controller('desafios')
-export class DesafiosController {}
+export class DesafiosController {
+  constructor(private readonly desafiosService: DesafiosService) {}
+
+  @Get()
+  async buscarDesafios(): Promise<Desafio[]> {
+    return this.desafiosService.buscarDesafios();
+  }
+
+  @Get(':id')
+  async buscarDesafioId(@Param('id') id: string): Promise<Desafio> {
+    return this.desafiosService.buscarDesafioId(id);
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  async inserirDesafio(
+    @Body() criarDesafioDto: CriarDesafioDto,
+  ): Promise<Desafio> {
+    return this.desafiosService.inserirDesafio(criarDesafioDto);
+  }
+
+  @Put(':id')
+  @UsePipes(ValidationPipe)
+  async atualizarDesafio(
+    @Param('id') id: string,
+    @Body() criarDesafioDto: CriarDesafioDto,
+  ): Promise<Desafio> {
+    return this.desafiosService.atualizarDesafio(id, criarDesafioDto);
+  }
+
+  @Delete(':id')
+  async deletarDesafio(@Param('id') id: string): Promise<void> {
+    this.desafiosService.deletarDesafio(id);
+  }
+}
--- src/desafios/desafios.service.ts
+++ src/desafios/desafios.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CriarDesafioDto } from './dtos/criar-desafio.dto';
+import { Desafio } from './interfaces/desafio.interface';
 
 @Injectable()
-export class DesafiosService {}
+export class DesafiosService {
+  constructor(
+    @InjectModel('Desafio') private readonly desafioModal: Model<Desafio>,
+  ) {}
+
+  async buscarDesafios(): Promise<Desafio[]> {
+    return this.desafioModal.find().exec();
+  }
+
+  async buscarDesafioId(_id: string): Promise<Desafio> {
+    const desafio = this.desafioModal.findOne({ _id }).exec();
+
+    if (!desafio) {
+      throw new NotFoundException('Desafio inexistente');
+    }
+
+    return desafio;
+  }
+
+  async inserirDesafio(criarDesafioDto: CriarDesafioDto): Promise<Desafio> {
+    const desafio = new this.desafioModal(criarDesafioDto);
+
+    return desafio.save();
+  }
+
+  async deletarDesafio(_id: string): Promise<any> {
+    await this.buscarDesafioId(_id);
+
+    return this.desafioModal.deleteOne({ _id }).exec();
+  }
+
+  async atualizarDesafio(
+    _id: string,
+    criarDesafioDto: CriarDesafioDto,
+  ): Promise<Desafio> {
+    await this.buscarDesafioId(_id);
+
+    return this.desafioModal.findOneAndUpdate({ _id }, criarDesafioDto).exec();
+  }
+}
